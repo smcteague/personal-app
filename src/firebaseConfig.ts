0713892845fad7dd3fbc159f89f2f6690ac4ad5c
Steve McTeague <smcteague@yahoo.com>
2022-12-12 13:33:44 -0600

    updated commit

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA4iUZ172hhuJ9ziF-Rnnux288pTNftvJc",
+    authDomain: "personal-app-11c95.firebaseapp.com",
+    projectId: "personal-app-11c95",
+    storageBucket: "personal-app-11c95.appspot.com",
+    messagingSenderId: "567935546572",
+    appId: "1:567935546572:web:8245b3338a46f1b07f7e69"
+  };
